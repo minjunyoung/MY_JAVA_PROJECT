54e45d1fb0f75e668f684b15bf1d1156de404168
User <User@DESKTOP-0KBA1LC>
2021-01-08 17:49:45 +0900

    workshop2 10110 민준영

---
+++ src/icehs/science/workshop2Pakage/VehicleDriver.java
@@ -0,0 +1,9 @@
+
+package icehs.science.workshop2Pakage;
+public class VehicleDriver {
+	public static void main(String[] args) {
+		VehicleManager vm = new VehicleManager();
+		vm.displayVehicles(">> 재고 목록 <<");
+		
+	}
+}
